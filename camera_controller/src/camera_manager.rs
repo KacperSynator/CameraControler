12611d769006e33c71001d8b1124be14c8aa4ce2
KacperSynator <kacper.synator@gmail.com>
2023-07-30 18:58:58 +0200

    camera_manager: create and move code from main

---
+++ camera_controller/src/camera_manager.rs
@@ -0,0 +1,61 @@
+use log::{error, info};
+use opencv::{prelude::*, videoio, Result};
+use std::error::Error;
+use std::fmt;
+
+const DEFAULT_CAMERA: i32 = 0;
+
+#[derive(Debug)]
+struct CameraManagerError(String);
+
+impl fmt::Display for CameraManagerError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Error: {}", self.0)
+    }
+}
+
+impl Error for CameraManagerError {}
+
+pub struct CameraManager {
+    camera: videoio::VideoCapture,
+}
+
+impl CameraManager {
+    pub fn new() -> Result<Self, Box<dyn Error>> {
+        let camera = videoio::VideoCapture::new(DEFAULT_CAMERA, videoio::CAP_ANY)?;
+        let opened = videoio::VideoCapture::is_opened(&camera)?;
+
+        if !opened {
+            let error_msg = String::from("Unable to open default camera!");
+            error!("{}", error_msg);
+            return Err(Box::<CameraManagerError>::new(CameraManagerError(
+                error_msg,
+            )));
+        }
+
+        Ok(Self { camera })
+    }
+
+    pub fn read_frame(&mut self) -> Result<Mat, Box<dyn Error>> {
+        let mut frame = Mat::default();
+
+        if let Err(err) = self.camera.read(&mut frame) {
+            let error_msg = format!("Failed to read frame from the camera: {:?}", err);
+            error!("{}", error_msg);
+            return Err(Box::<CameraManagerError>::new(CameraManagerError(
+                error_msg,
+            )));
+        }
+
+        if frame.size()?.width <= 0 {
+            let error_msg = String::from("Failed to capture frame, width <= 0");
+            error!("{}", error_msg);
+            return Err(Box::<CameraManagerError>::new(CameraManagerError(
+                error_msg,
+            )));
+        }
+
+        info!("Frame read successfully");
+        Ok(frame)
+    }
+}
--- camera_controller/src/main.rs
+++ camera_controller/src/main.rs
@@ -1,31 +1,17 @@
-use opencv::{imgcodecs, prelude::*, videoio, Result};
+use log::info;
+use opencv::{imgcodecs, Result};
 use std::error::Error;
-use log::{error, info};
+
+use camera_controller::camera_manager::CameraManager;
 
 fn main() -> Result<(), Box<dyn Error>> {
     pretty_env_logger::init();
 
-    // Open the camera
-    let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY)?; // 0 is the default camera
-    let opened = videoio::VideoCapture::is_opened(&cam)?;
-	if !opened {
-        error!("Unable to open default camera!");
-	}
+    let mut camera_manager = CameraManager::new()?;
+    let frame = camera_manager.read_frame()?;
 
-    // Create a Mat to store the captured frame
-    let mut frame = Mat::default();
-
-    // Read the frame from the camera
-    cam.read(&mut frame).unwrap();
-
-    // Check if the frame is valid
-    if frame.size()?.width > 0 {
-        // Save the frame as an image file
-        imgcodecs::imwrite("output.jpg", &frame, &opencv::types::VectorOfi32::new()).unwrap();
-        info!("Image saved successfully");
-    } else {
-        error!("Failed to capture frame from the camera");
-    }
+    imgcodecs::imwrite("output.jpg", &frame, &opencv::types::VectorOfi32::new())?;
+    info!("Image saved successfully");
 
     Ok(())
 }
